3da6693773d710931d280d124736b9ad12a94694
Ishara <sandunchanaka@gmail.com>
2024-06-27 11:36:18 +0530

    logo update

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,6 +1,7 @@
 <nav class="navbar bg-body-tertiary mb-5">
-    <div class="container-fluid">
+    <div class="container-fluid text-center">
         <a class="navbar-brand m-auto" href="#">
+            <img src="./frontend/images/logo.png" class="d-inline-block align-top" alt="">
             Budget Website
         </a>
     </div>
